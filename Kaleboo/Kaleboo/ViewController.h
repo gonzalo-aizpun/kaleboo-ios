22b39cd2b7909c23d9d6c16e1ce8a109d903cdf9
OLX - Andres Ciaño <andres.ciano@olx.com>
2015-05-29 10:40:32 -0300

    Initial Setup

---
+++ Kaleboo/Kaleboo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kaleboo
+//
+//  Created by OLX - Andres Ciaño on 5/29/15.
+//  Copyright (c) 2015 Kaleboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Kaleboo/Kaleboo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Kaleboo
+//
+//  Created by OLX - Andres Ciaño on 5/29/15.
+//  Copyright (c) 2015 Kaleboo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
